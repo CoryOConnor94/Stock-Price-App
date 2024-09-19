59eed204ad4714cfb56bb9f8e40ec092a4fc23c0
Cory <CoryOConnor1994@gmail.com>
2024-09-19 20:41:58 +0100

    Update stock_price_app.py
    Updated UI

--- stock_price_app.py
+++ stock_price_app.py
@@ -3,20 +3,24 @@ from tkinter import *
 from tkinter import messagebox
 import matplotlib.pyplot as plt
 
-
+BACKGROUND = '#040404'
+DARK_BLUE = '#0a1e33'
 def get_stock_price():
     stock_symbol = entry.get().upper()
     ticker_data = yf.Ticker(stock_symbol)
 
     latest_price = ticker_data.history(period='1d')['Close'].iloc[-1]
-    messagebox.showinfo(f"Stock Price: {stock_symbol}",f"Latest price: {latest_price}")
+    # Display the price using the canvas's create_text method
+    canvas_text.create_text(150, 20, text=f"Latest {stock_symbol} Close Price: ${latest_price:.2f}",
+                            font=('Arial', 12), fill='blue')
 
-    # messagebox.showerror("Error","Unable to fetch data. Please check the stock symbol.")
+    plot_stock(ticker_data)
 
+def plot_stock(ticker_data):
     # Plot the closing prices over the selected time period
     historical_data = ticker_data.history(period='1mo')
     historical_data['Close'].plot(figsize=(10, 6))
-    plt.title(f'{stock_symbol} Stock Price')
+    plt.title(f'{ticker_data.info.get("shortName")} Stock Price')
     plt.xlabel('Date')
     plt.ylabel('Close Price')
     plt.grid(True)
@@ -25,16 +29,29 @@ def get_stock_price():
 
 window = Tk()
 window.title("Stock Price App")
-window.config(padx=20, pady=20)
+window.geometry("800x600")
+# Load the image file
+bg_image = PhotoImage(file="background.png")
 
-label = Label(window, text="Enter Stock Ticker:")
-label.pack()
+# Create a Canvas widget
+canvas = Canvas(window, width=800, height=600, highlightthickness=0)
+canvas.pack(fill="both", expand=True)
 
-entry = Entry(window)
-entry.pack()
+# Add the image to the canvas
+canvas.create_image(0, 0, image=bg_image, anchor="nw")
 
-button = Button(window, text="Get Price", command=get_stock_price)
-button.pack()
+# Create a Canvas to display text
+canvas_text = Canvas(canvas, width=300, height=40, bg=DARK_BLUE, highlightthickness=0)
+canvas_text.pack(side=BOTTOM)
+
+label = Label(canvas, text="Enter Stock Ticker:")
+label.pack(side=TOP, anchor='nw', padx=5, pady=5)  # Top-left (northwest)
+
+entry = Entry(canvas)
+entry.pack(side=TOP, anchor='nw', padx=5, pady=5)
+
+button = Button(canvas, text="Get Price", command=get_stock_price)
+button.pack(side=TOP, anchor='nw', padx=5, pady=5)
 
 
 
